b1f7dd514706e3937493ba6b399630785c2c7521
Nati70-1 <Natiaabaydam@gmail.com>
2022-02-19 14:13:56 -0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("hello world!"),
+            centerTitle: true,
+            backgroundColor: Colors.purple[500],
+          ),
+          body: const Center(
+            child: Text("middle screen text"),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Text("tap"),
+            backgroundColor: Colors.blue[600],
+          ),
+        ),
+      ),
+    );
